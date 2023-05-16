db8110624fa7e650e275f7122212b863c3751645
mike <cspkno005@gmail.com>
2023-05-17 00:20:28 +0800

    css test

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<style>
+.text-3xl {
+  font-size: 1.875rem;
+  line-height: 2.25rem;
+}
+
+.underline {
+  text-decoration-line: underline;
+}
+
+.font-bold {
+  font-weight: 700;
+}
+</style>
