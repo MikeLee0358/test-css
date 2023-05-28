5482c808cf0629e3e8782bdb287856cae43d8209
mike <cspkno005@gmail.com>
2023-05-29 00:24:06 +0800

    刪除重複ＣＳＳ

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['@/assets/preflight.css']
+    css: ['@/assets/preflight.css'],
+    experimental: {
+        inlineSSRStyles: false,
+    }
 })
